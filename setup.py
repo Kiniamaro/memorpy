df121a4aaa946395a13b6098fcf7d7c0e73c8477
Kiniamaro <vdyotte@gmail.com>
2018-11-15 14:10:58 -0500

    change setup to allow python 3

--- setup.py
+++ setup.py
@@ -9,20 +9,19 @@ except ImportError:
 
 if sys.version_info < (2, 5):
     sys.exit("requires python 2.5 and up")
-if sys.version_info[0] == 3:
-	sys.exit("not compatible python 3")
+
 
 here = os.path.dirname(__file__)
 
 exec(open(os.path.join(here, 'memorpy', 'version.py')).read())
 setup(name = "memorpy",
-    version = version_string, #@UndefinedVariable,
+    version = version_string, #  noqa
     description = "Python library using ctypes to search/edit windows/linux programs memory",
-    author = "Nicolas VERDIER",
-    author_email = "contact@n1nj4.eu",
-    maintainer_email = "contact@n1nj4.eu",
+    author = "Victor Dyotte",
+    author_email = "vdyotte@gmail.com",
+    maintainer_email="vdyotte@gmail.com",
     license = "BSD",
-    url = "https://github.com/n1nj4sec/memorpy",
+    url = "https://github.com/Kiniamaro/memorpy",
     packages = [
         'memorpy',
     ],
